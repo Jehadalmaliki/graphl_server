f714aa92353cb382afff691a8c7fdfd236efa6bc
Jehadalmaliki <jehad@fastn.ai>
2024-09-22 17:05:23 +0300

    feat: init file

---
+++ Schema.js
@@ -0,0 +1,19 @@
+export const typeDefs=`#graphql
+  type Game {
+    id: ID!
+    title: String 
+    platform:[String!]! 
+}
+
+type Review {
+    id: ID!
+    rating: Int! 
+    content: String!
+}
+
+type Author {
+    id: ID!
+    name: String!
+    verified:Boolean!q
+}
+`
